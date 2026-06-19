4570affe794f9ef57b3322ddf86d2a4a19e70a19
Savitha66 <itzsuni798@gmail.com>
2026-06-19 16:14:33 +0530

    fix day1 folder

---
+++ day1/index.dart
@@ -0,0 +1,16 @@
+void main() {
+  //Entey point of the code
+  var name = "Savitha";
+  var age = 12;
+  print(name);
+  print(age);
+  double price = 99.99;
+  print(price);
+
+  bool isLogged = true;
+  print(isLogged);
+
+  dynamic data = "savi";
+  data = 66;
+  print(data);
+}
